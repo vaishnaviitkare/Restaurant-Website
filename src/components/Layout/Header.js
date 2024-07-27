ca92399bdf7d85812d709e9d9ebb772b806137a9
vaishnaviitkare <vaishuitkare@gmail.com>
2024-07-27 21:49:51 +0530

    header is created

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeadCartButton from "./HeaderCartButton";
+const Header=()=>{
+    return(
+        <React.Fragment>
+       <header className={classes.head}>
+        <h1>ReactMeals</h1>
+       <HeadCartButton/>
+       </header>
+       {/*here in main-image name - is present so we can't use .main-image here we use in []*/}
+       <div className={classes['main-image']}>
+       <img src={logo} alt="A table full of delicious food!"/>
+       </div>
+       </React.Fragment>
+
+    );
+
+}
+export default Header;
